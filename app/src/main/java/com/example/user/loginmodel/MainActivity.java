b6b80822355515dc276c7f04b66732a25fe6d06b
LiuYang <1005859278@qq.com>
2018-11-16 18:00:42 +0800

    加入okhttp

--- app/src/main/java/com/example/user/loginmodel/MainActivity.java
+++ app/src/main/java/com/example/user/loginmodel/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.user.loginmodel;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Button;
+
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import okhttp3.Call;
+import okhttp3.Request;
 
 /**
  * @author : LiuYang
@@ -29,6 +34,22 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.login)
     public void onViewClicked() {
-        startActivity(new Intent(this, LoginActivity.class));
+        String url = "http://192.168.110.8:3000/mock/39/api/infra-uuv/v0.1/users/login";
+        OkHttpUtils
+                .get()
+                .url(url)
+                .build()
+                .execute(new StringCallback() {
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+                        Log.d("MainActivity", "onError: " + e.getMessage());
+                    }
+
+                    @Override
+                    public void onResponse(String response, int id) {
+                        Log.d("MainActivity", "onResponse: " + response);
+                    }
+                });
+//        startActivity(new Intent(this, LoginActivity.class));
     }
 }
